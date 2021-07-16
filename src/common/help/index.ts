e38290ef38f5c6e8810d38b6064db0bdb77d1ea4
xencodes <xencodes@icloud.com>
2021-07-16 21:12:41 +0530

    docs: add help command

--- src/common/help/index.ts
+++ src/common/help/index.ts
@@ -2,11 +2,44 @@ import { Composer } from "grammy";
 
 const composer = new Composer();
 
-composer.command("help", (ctx) =>
-  ctx.reply(`Head over to my [website](https://snzy.in) to get some help.`, {
-    parse_mode: "Markdown",
-    disable_web_page_preview: true,
-  })
-);
+import glob from "glob";
+import path from "path";
+
+const modulesFolder = path.join(__dirname, "../../modules");
+
+let ModuleHelp = {};
+
+glob(modulesFolder + "/**/meta.*", (er, files) => {
+  files.map((file) => {
+    let mod = require(file);
+    if (!mod.default) return;
+    mod = mod.default;
+
+    ModuleHelp[mod.name] = mod;
+
+    // ModuleHelp[d.name] = d.defaults.help;
+  });
+});
+
+composer.command("help", (ctx) => {
+  let message = "";
+
+  if (ctx.chat.type === "private") {
+    Object.keys(ModuleHelp).map((name) => {
+      const description = ModuleHelp[name].description;
+      const usage = ModuleHelp[name].usage;
+
+      message += `*${name}*\n`;
+      message += `_${description}_\n`;
+      message += `Usage:  ${usage.map((i) => "`" + i + "`").join(" ")}\n\n`;
+    });
+
+    ctx.reply(message, {
+      parse_mode: "Markdown",
+      disable_web_page_preview: true,
+    });
+    return;
+  }
+});
 
 export default composer;
