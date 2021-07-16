03603f2dec90f5e8a6709f0d8cca02ee842997ac
xencodes <xencodes@icloud.com>
2021-07-16 16:59:19 +0530

    feat: add image search [unsplash[

---
+++ src/modules/images/index.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { Context } from "telegraf";
+import meta from "./meta";
+
+const Unsplash = async (ctx: Context, params?: any) => {
+  const { UNSPLASH: CLIENT_ID } = process.env;
+  if (!params) return;
+
+  ctx.telegram.sendChatAction(ctx.chat.id, "upload_photo");
+
+  axios
+    .get(
+      `https://api.unsplash.com/search/photos?page=1&query=${params}&client_id=${CLIENT_ID}`
+    )
+    .then(({ data }) => {
+      if (!data) return;
+
+      const { results } = data;
+
+      if (results.length) {
+        ctx.reply(`Not found.`);
+        return;
+      }
+      const randomResult = results[Math.floor(Math.random() * results.length)];
+      const result = results[randomResult];
+
+      ctx.replyWithPhoto({ url: result.urls.regular });
+    })
+    .catch(() => {
+      ctx.reply(`Error occurred.`);
+    });
+};
+
+export default {
+  handler: Unsplash,
+  match: /^\?img (.*)/,
+  ...meta,
+};
---
+++ src/modules/images/meta.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "Images",
+  description: "Get images from Unsplash.",
+  usage: ["?img cats", "?img macbook"],
+};
